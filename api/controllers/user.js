8d7a655ea566a4b650a2dfbad1c33c1edb1b63e3
afreediz <afreedisulfiker@gmail.com>
2024-04-09 17:41:49 +0530

    user controllers setted

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -4,21 +4,58 @@ const CustomError = require('../utils/CustomError')
 const User = require('../models/user')
 
 const getUser = asyncErrorHandler(async(req, res)=>{
-    //
-})
-const orders = asyncErrorHandler(async(req, res)=>{
-    //
+    res.status(200).json({
+        success:true,
+        message:"Authorized",
+        authorized:true
+    })
 })
 const profile = asyncErrorHandler(async(req, res)=>{
+    const id = req.user._id
+    
+    const user = await User.findById(id)
+    
+    res.status(200).json({
+        success:true,
+        message:"Profile",
+        user
+,    })
+})
+const updateProfile = asyncErrorHandler(async(req, res)=>{
+    const id = req.user._id
+    
+    const updated_user = await User.findByIdAndUpdate(id, {...req.body},{runValidators:true, new:true})
+    
+    res.status(200).json({
+        success:true,
+        message:"Profile updated",
+        user:updated_user
+    })
+})
+const deleteProfile = asyncErrorHandler(async(req, res)=>{
+    const id = req.user._id
+    console.log(req.user);
+    await User.findByIdAndDelete(id)
+    
+    res.status(200).json({
+        success:true,
+        message:"User profile deleted successfully",
+    })
+})
+const orders = asyncErrorHandler(async(req, res)=>{
     //
 })
 
 // admin operations
 const getAdmin = asyncErrorHandler(async(req, res)=>{
-    //
+    res.status(200).json({
+        success:true,
+        message:"Authorized",
+        authorized:true
+    })
 })
 const allOrders = asyncErrorHandler(async(req, res)=>{
     //
 })
 
-module.exports = {getUser, getAdmin, profile, orders, allOrders }
+module.exports = {getUser, getAdmin, profile, orders, allOrders, updateProfile, deleteProfile }
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,13 +1,15 @@
 const router = require('express').Router()
-const { getUser, getAdmin, orders, allOrders, profile } = require('../controllers/user')
+const { getUser, getAdmin, orders, allOrders, profile, updateProfile, deleteProfile } = require('../controllers/user')
 const { isAuthenticated, isAdmin } = require('../middlewares/isAuth')
 
-router.get('/get-user', getUser)
-router.get('/get-admin', getAdmin)
-router.get('/orders', orders)
-router.get('/profile', profile)
+router.get('/get-user', isAuthenticated, getUser) // done
+router.get('/profile', isAuthenticated, profile) // done
+router.put('/profile', isAuthenticated, updateProfile) // done
+router.delete('/profile', isAuthenticated, deleteProfile) // done
+router.get('/orders', isAuthenticated, orders)
 
 // admin routes
-router.get('/all-orders', allOrders)
+router.get('/get-admin', isAuthenticated, isAdmin, getAdmin) // done
+router.get('/all-orders', isAuthenticated, isAdmin,allOrders) // done
 
 module.exports = router
