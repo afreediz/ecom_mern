a0dd2487230632a154c8c8b03fba75a71fb62f88
afreediz <afreedisulfiker@gmail.com>
2024-04-15 00:17:21 +0530

    profile page created

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>
-      profile
+    <div className=' text-2xl'>
+      <div className=" my-2">
+        <label className='block' htmlFor="name">Name</label>
+        <input type="text" id='name' value="afreedi" placeholder='Enter your name' />
+      </div>
+      <div className=" my-2">
+        <label className="block" htmlFor="email">Email</label>
+        <input type="text" id="email" value="afreedi@gmail.com" placeholder='Enter your email' />
+      </div>
+      <div className=" my-2">
+        <label className="block" htmlFor="phone">Phone</label>
+        <input type="text" id="phone" value="9090909090" placeholder='Enter your Phone' />
+      </div>
+      <div className=" my-2">
+        <label className="block" htmlFor="address">Address</label>
+        <input type="text" id="address" value="5/1 house" placeholder='Enter your Address' />
+      </div>
+      <button className='py-2 px-5 bg-green-600 text-white font-medium'>Update</button>
     </div>
   )
 }
